c03c130b8c618dcd536b5f1fdee09e18ba463bb2
Falco Girgis <falco.girgis@elysianshadows.com>
2015-03-02 19:43:09 -0600

    Put basic TCP code into realADSB module to move over to the device and start playing with it.

--- realADSB.cpp
+++ realADSB.cpp
@@ -1,7 +1,20 @@
 #include "realADSB.h"
 #include <pthread.h>
 #include <stdio.h>
-# include <stdlib.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
 
 RealADSB::~RealADSB()
 {/* Last thing that main() should do */
@@ -23,12 +36,54 @@ bool    RealADSB::getLongitude(float& lon) { return true; }
 bool    RealADSB::getTrack(int& track) { return true; }
 bool    RealADSB::getDeltaTime(int& deltaTime) { return true; }
 
-void *PrintHello(void *threadid)
+void *ADSBClientThread(void *threadid)
 {
    long tid;
    tid = (long)threadid;
    printf("ADS-B thread #%ld!\n", tid);
    pthread_exit(NULL);
+
+
+
+   int sockfd, portno, n;
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+
+   char buffer[256];
+   if (argc < 3) {
+      fprintf(stderr,"usage %s hostname port\n", argv[0]);
+      exit(0);
+   }
+   portno = atoi(argv[2]);
+   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+   if (sockfd < 0)
+       error("ERROR opening socket");
+   server = gethostbyname(argv[1]);
+   if (server == NULL) {
+       fprintf(stderr,"ERROR, no such host\n");
+       exit(0);
+   }
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr,
+        (char *)&serv_addr.sin_addr.s_addr,
+        server->h_length);
+   serv_addr.sin_port = htons(portno);
+   if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0)
+       error("ERROR connecting");
+   printf("Please enter the message: ");
+   bzero(buffer,256);
+   fgets(buffer,255,stdin);
+   n = write(sockfd,buffer,strlen(buffer));
+   if (n < 0)
+        error("ERROR writing to socket");
+   bzero(buffer,256);
+   n = read(sockfd,buffer,255);
+   if (n < 0)
+        error("ERROR reading from socket");
+   printf("%s\n",buffer);
+   close(sockfd);
+   return 0;
 }
 
 bool RealADSB::initialize()
